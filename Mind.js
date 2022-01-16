777c8693f072052eeea802de13871b6653a6f165
Franklin Lopes <franklinlopes83@hotmail.com>
2022-01-16 18:17:21 -0300

    Criar consumo de API Rick and Morty

--- Mind.js
+++ Mind.js
@@ -1,5 +1,28 @@
 const imagem = document.querySelector('img');
 const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
-const especie = document.querySelector('especie');
-const status = document.querySelector('atualmente');
+const especie = document.querySelector('#especie');
+const atualmente = document.querySelector('#status');
+
+gerarValorAleatorio = () => {
+    return Math.floor(Math.random() * 3 - 0 + 1);
+}
+
+pegarPersonagem = () => {
+    let numeroAleatorio = gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((Response) => Response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.species;
+        atualmente.innerHTML = data.status;
+    });
+}
+
+botao.onclick = pegarPersonagem;
